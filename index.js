f1db78cc2614cd8d636dc00c55b5b3d091976b65
RedLime <redlimerl@gmail.com>
2023-04-29 17:40:41 +0900

    add split path feature

--- index.js
+++ index.js
@@ -12,49 +12,56 @@ async function run() {
 
     try {
         const basePath = core.getInput('base-path');
+        const langFilesPath = core.getInput('lang-files-path');
+        const editablePath = core.getInput('editable-files-path');
         const fileSuffix = core.getInput('end-with');
         const editableSuffix = core.getInput('editable-suffix');
         const backupSuffix = core.getInput('backup-suffix');
         const defaultName = core.getInput('default-language');
-    
-        core.info('loading base-path files...');
-        const pathFiles = fs.readdirSync(path.join(basePath));
-        core.info(`loaded ${pathFiles.length} base-path files.`);
-    
-        for (const pathName of pathFiles) {
-            if (!pathName.endsWith(fileSuffix)) return;
-            
-            let finalizeName = pathName.substring(0, pathName.length - fileSuffix.length);
-            
-            const isEditable = finalizeName.endsWith(editableSuffix);
-            if (isEditable) finalizeName = finalizeName.substring(0, finalizeName.length - editableSuffix.length);
-            
-            const isBackup = finalizeName.endsWith(backupSuffix);
-            if (isBackup) finalizeName = finalizeName.substring(0, finalizeName.length - backupSuffix.length);
-    
-            const isDefault = !isBackup && finalizeName == defaultName;
-    
-            core.info(`found '${pathName}'. (editable: ${isEditable}, default: ${isDefault}, backup: ${isBackup})`);
 
-            core.info(`reading '${pathName}'...`);
-            const langData = JSON.parse(fs.readFileSync(path.join(basePath, pathName), 'utf8'));
-            core.info(`read '${pathName}' json. lang keys: ${Object.keys(langData).length}`);
-
-            if (isDefault) {
-                defaultData = langData;
-            } else if (isBackup) {
-                backupData = langData;
-            } else {
-                if (editableExist[finalizeName] == undefined) editableExist[finalizeName] = false;
+        const updatePathFiles = (fileNames, isEditablePath) => {
+            for (const pathName of fileNames) {
+                if (!pathName.endsWith(fileSuffix)) return;
                 
-                if (isEditable) {
-                    languageData[finalizeName] = langData;
-                    editableExist[finalizeName] = true;
+                let finalizeName = pathName.substring(0, pathName.length - fileSuffix.length);
+                
+                const isEditable = finalizeName.endsWith(editableSuffix);
+                if (isEditable) finalizeName = finalizeName.substring(0, finalizeName.length - editableSuffix.length);
+                
+                const isBackup = finalizeName.endsWith(backupSuffix);
+                if (isBackup) finalizeName = finalizeName.substring(0, finalizeName.length - backupSuffix.length);
+        
+                const isDefault = !isBackup && finalizeName == defaultName;
+        
+                core.info(`found '${pathName}'. (editable: ${isEditable}, default: ${isDefault}, backup: ${isBackup})`);
+    
+                core.info(`reading '${pathName}'...`);
+                const langData = JSON.parse(fs.readFileSync(path.join(basePath, pathName), 'utf8'));
+                core.info(`read '${pathName}' json. lang keys: ${Object.keys(langData).length}`);
+    
+                if (isDefault && !isEditablePath) {
+                    defaultData = langData;
+                } else if (isBackup && !isEditablePath) {
+                    backupData = langData;
                 } else {
-                    oldLangData[finalizeName] = langData;
+                    if (editableExist[finalizeName] == undefined) editableExist[finalizeName] = false;
+                    
+                    if (isEditable == isEditablePath) continue;
+
+                    if (isEditable) {
+                        languageData[finalizeName] = langData;
+                        editableExist[finalizeName] = true;
+                    } else {
+                        oldLangData[finalizeName] = langData;
+                    }
                 }
             }
         }
+    
+        core.info('loading lang files...');
+        updatePathFiles(fs.readdirSync(path.join(basePath, langFilesPath)), false);
+        updatePathFiles(fs.readdirSync(path.join(basePath, editablePath)), true);
+        core.info(`loaded ${pathFiles.length} lang files.`);
 
         if (!defaultData) {
             core.setFailed('Failed to load default lang file.');
@@ -109,13 +116,13 @@ async function run() {
                 resultData[key] = value;
             }
             
-            fs.writeFileSync(path.join(basePath, langName + fileSuffix), JSON.stringify(resultData, null, 4), 'utf8');
-            fs.writeFileSync(path.join(basePath, langName + editableSuffix + fileSuffix), JSON.stringify(languageData[langName], null, 4), 'utf8');
+            fs.writeFileSync(path.join(basePath, langFilesPath, langName + fileSuffix), JSON.stringify(resultData, null, 4), 'utf8');
+            fs.writeFileSync(path.join(basePath, editablePath, langName + editableSuffix + fileSuffix), JSON.stringify(languageData[langName], null, 4), 'utf8');
             core.info(`done with update '${langName}'.`);
         }
 
         core.info(`backup old default strings...`);
-        fs.writeFileSync(path.join(basePath, defaultName + backupSuffix + fileSuffix), JSON.stringify(defaultData, null, 4), 'utf8');
+        fs.writeFileSync(path.join(basePath, langFilesPath, defaultName + backupSuffix + fileSuffix), JSON.stringify(defaultData, null, 4), 'utf8');
         core.info(`done with backup default strings.`);
 
         core.info(`complete!`);
